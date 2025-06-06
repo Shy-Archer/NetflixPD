710b097530cb4b4cddb9ac6630ede05223ca0682
Just_Bows <lukaszwalicki2002@gmail.com>
2025-06-07 01:25:39 +0200

    modify scripts

--- jdbc.sh
+++ jdbc.sh
@@ -1,7 +1,7 @@
 source ./env.sh
 
 # Ścieżka do JAR-a aplikacji (bez spacji!)
-APP_JAR="out/artifacts/JdbcConsumer/JdbcConsumer.jar"
+APP_JAR="out/artifacts/JDBCConsumer/JDBCConsumer.jar"
 
 # Ścieżka do sterownika PostgreSQL (upewnij się, że plik istnieje w tym katalogu)
 PG_JAR="postgresql-42.6.0.jar"
